766a284356e5346279b7d488141dfd5ae06e7f73
Ryan H <ryanheal89@gmail.com>
2022-03-12 00:34:26 -0800

    Updates

---
+++ precache-manifest.e69ca37b7d1c223393472a9c1ca1f8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627f8d5de836c8c845a06ceb9d43715e",
+    "url": "/react-pokemon-wiki/index.html"
+  },
+  {
+    "revision": "60d64d3882397863b833",
+    "url": "/react-pokemon-wiki/static/css/2.900186da.chunk.css"
+  },
+  {
+    "revision": "b3948eb46a663ca1f343",
+    "url": "/react-pokemon-wiki/static/css/main.a5ea9cfa.chunk.css"
+  },
+  {
+    "revision": "60d64d3882397863b833",
+    "url": "/react-pokemon-wiki/static/js/2.fb2bafdf.chunk.js"
+  },
+  {
+    "revision": "b3948eb46a663ca1f343",
+    "url": "/react-pokemon-wiki/static/js/main.4de270a3.chunk.js"
+  },
+  {
+    "revision": "dfeacf6e178a3447e050",
+    "url": "/react-pokemon-wiki/static/js/runtime~main.6bd2b388.js"
+  }
+]);
